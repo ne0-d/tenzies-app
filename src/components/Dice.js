61096c5db1dbf03315a5f6bbc9651bf7e4f2fbea
ne0-d <dhruvbhati2001@gmail.com>
2022-08-02 10:42:57 +0530

    Added dice componnet

---
+++ src/components/Dice.js
@@ -0,0 +1,7 @@
+export default function Dice(props){
+    return(
+    <div class="dice">        
+        <p>1</p>
+    </div>
+    );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Dice from "./Dice"
+export default function Main(){
+    return(
+        <main class="main"> 
+        <div class="dice--container">
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+            <Dice />
+        </div>       
+        </main>
+    );
+} 
